12127fdb1702ae5a48d70001512b3c118dd676d9
Kenny Nguyen <sokenns@gmail.com>
2025-03-29 23:35:54 -0700

    Add season struct

--- defs/defs.go
+++ defs/defs.go
@@ -11,4 +11,12 @@ type Series struct {
 	Premiere_date *time.Time `json:"premiere_date"`
 }
 
+type Season struct {
+	Season_id     int        `json:"-"`
+	Series_id     string     `json:"series_id"`
+	Season_number int        `json:"season_number"`
+	Premiere_date *time.Time `json:"premiere_date"`
+	Finale_date   *time.Time `json:"finale_date"`
+}
+
 // omitempty
